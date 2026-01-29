ccc462b46bcda3b9224cf4c3f19a92a36815c1b0
govin-raaj <govin30raaj@gmail.com>
2026-01-29 09:41:50 +0530

    portfolio done

--- vite-project/src/components/navbar.jsx
+++ vite-project/src/components/navbar.jsx
@@ -2,7 +2,7 @@
 function Navbar() {
 
   return (
-    <nav className="flex  bg-purple-600 text-white justify-between px-2 md:px-4 py-2 md:py-4 rounded-2xl mx-2 my-2 items-center">
+    <nav className="flex  bg-[#621bbd] text-white justify-between px-2 md:px-4 py-2 md:py-4 rounded-2xl mx-2 my-2 items-center">
       <h1 className="text-xl font-bold">My Portfolio</h1>
 
       <div className="md:flex md:gap-5 font-medium hidden">
