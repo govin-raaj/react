780d760e9b4b7dedb47f4df49097cb302665e3a6
govin-raaj <govin30raaj@gmail.com>
2026-01-30 16:31:06 +0530

    done

---
+++ counterapp/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const [counters, setCounters] = useState([{value:0,step:1}, {value:0,step:1}])
+
+  const increment = (index) => {
+    setCounters(prev=> prev.map((counter,i)=>{
+      return i===index? {...counter,value:counter.value+counter.step}: counter
+    }))
+  }
+
+  const decrement = (index) => {
+    setCounters(prev=>prev.map((counter,i)=>{
+      return i===index &&counter.value>0? {...counter,value:counter.value-counter.step}:counter
+    }))
+  }
+
+  const resetAll = () => {
+    setCounters(prev =>
+    prev.map(counter => ({ ...counter, value: 0 ,step:1}))
+  )
+  }
+
+
+  return (
+    <>
+      <div className="min-h-screen w-full bg-gradient-to-b from-gray-900 to-gray-600 text-white relative overflow-hidden flex items-center justify-center" >
+
+        <div className="bg-white p-6 rounded-xl  sm:w-[500px]">
+          <h1 className='text-2xl font-bold text-center mb-4 text-black'>Counter dashboard</h1>
+
+          {counters.map((counter, index) => (
+            <div key={index} className="text-black flex justify-between ring-3 rounded-xl px-5 py-4 m-3 overflow-hidden">
+              <h1 className='text-black font-extrabold'>{counter.value}</h1>
+
+              <input
+                type="number"
+                value={counter.step}
+                onChange={(e) => {setCounters(prev=>prev.map((c,i)=>i===index?{...c,step:Number(e.target.value)}:c))}}
+                className=" border px-2 py-2  text-black rounded-xl"
+                placeholder="Increment value"
+              />
+
+              <div className='flex gap-5'>
+                <button className='bg-amber-500 px-4  rounded-xl font-bold' onClick={() => increment(index)}>+</button>
+                <button className='bg-red-600  px-4  rounded-xl  font-bold' onClick={() => decrement(index)}>-</button>
+              </div>
+            </div>
+          ))}
+
+          <button
+            onClick={resetAll}
+            className="w-full bg-blue-600 text-white py-2 rounded-xl font-bold mb-4 hover:bg-blue-700"
+          >
+            Reset All
+          </button>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
